8e8a5892001574ce7bc1affda50234cbaf11b4c1
valentichu <valentichu@foxmail.com>
2017-09-04 21:30:05 +0800

    2017/09/04

--- src/main/java/com/valentichu/server/core/mapper/UserMapper.java
+++ src/main/java/com/valentichu/server/core/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package com.valentichu.server.core.mapper;
 
+import com.valentichu.server.core.domain.CartItem;
 import com.valentichu.server.core.domain.User;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 用户相关数据库接口
@@ -47,6 +50,18 @@ public interface UserMapper {
                           @Param("productId") Integer productId,
                           @Param("productNum") Integer productNum);
 
+    /**
+     * 更新购物车已有商品的选中状态
+     *
+     * @param userName  用户名
+     * @param productId 商品Id
+     * @param checked   选中状态
+     * @return 影响的行数，应该为1
+     */
+    int updateCartItemChecked(@Param("userName") String userName,
+                              @Param("productId") Integer productId,
+                              @Param("checked") Integer checked);
+
 
     /**
      * 取得购物车中的商品数量
@@ -57,5 +72,32 @@ public interface UserMapper {
      */
     Integer getCartItemNum(@Param("userName") String userName,
                            @Param("productId") Integer productId);
+
+    /**
+     * 取得购物车中的商品
+     *
+     * @param userName 用户名
+     * @return 购物车列表，可能为Null，需要对返回值进行判断
+     */
+    List<CartItem> listCart(@Param("userName") String userName);
+
+    /**
+     * 更新购物车中的商品选中状态
+     *
+     * @param userName 用户名
+     * @param checked  选中or不选中
+     * @return 影响的行数
+     */
+    Integer updateCartChecked(@Param("userName") String userName,
+                              @Param("checked") Integer checked);
+
+    /**
+     * 从购物车中删除商品
+     *
+     * @param userName  用户名
+     * @param productId 商品Id
+     */
+    void deleteCartItem(@Param("userName") String userName,
+                        @Param("productId") Integer productId);
 }
 
--- src/main/java/com/valentichu/server/core/service/impl/UserServiceImpl.java
+++ src/main/java/com/valentichu/server/core/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.valentichu.server.core.service.impl;
 
+import com.valentichu.server.core.domain.CartItem;
 import com.valentichu.server.core.mapper.UserMapper;
 import com.valentichu.server.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 用户Service的实现
@@ -29,5 +32,29 @@ public class UserServiceImpl implements UserService {
             cartItemNum++;
             userMapper.updateCartItemNum(userName, productId, cartItemNum);
         }
+    }
+
+    @Override
+    public List<CartItem> listCartItem(String userName) {
+        return userMapper.listCart(userName);
+    }
+
+    @Override
+    public void updateCartItem(String userName, int productId, String action, int value) {
+        if (action.equals("num")) {
+            userMapper.updateCartItemNum(userName, productId, value);
+        } else {
+            userMapper.updateCartItemChecked(userName, productId, value);
+        }
+    }
+
+    @Override
+    public void updateCartChecked(String userName, int checked) {
+        userMapper.updateCartChecked(userName, checked);
+    }
+
+    @Override
+    public void deleteCartItem(String userName, int productId) {
+        userMapper.deleteCartItem(userName, productId);
     }
 }
